5936e6bb1c90a7393eec0f8e1e24aebd18c51fdb
nagendra <nagendrasha191@gmail.com>
2024-06-21 14:48:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import { Grid } from '@mui/material';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Grid container sx={{ display:'flex',justifyContent:'center' }}>
+      <Grid item lg={4}>
+        <Header/>
+
+      </Grid>
+    </Grid>
+    </>
   );
 }
 
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,17 @@
+import { Grid } from '@mui/material'
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <>
+    <Grid container sx={{ display:'flex',justifyContent:'center' }} >
+        <Grid item lg={6}>
+            
+
+        </Grid>
+    </Grid>
+    </>
+  )
+}
+
+export default Homepage
